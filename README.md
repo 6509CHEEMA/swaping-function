e6384e0da4fa352788c86c7c547d8835303a3960
6509CHEEMA <88336801+6509CHEEMA@users.noreply.github.com>
2021-09-05 17:46:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# swaping-function
+# SAtemplate
---
+++ sketch.js
@@ -0,0 +1,6 @@
+var a=prompt(`enter the first variable`);
+var b=prompt(`enter the second variable`);
+[a,b]=[b,a]
+console.log(`the value of a after swaping is ${a}`)
+console.log(`the value of b after swaping is ${b}`)
+console.log("hello");
